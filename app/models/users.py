3ac7a6af1288bc0cde188305ea2332fcb02475d7
AmelieDogan <dogan.amelie@gmail.com>
2025-03-31 02:03:50 +0200

    Correction du fichier user.py qui n'a pas été correctement fusionné automatiquement lors du dernier merge

--- app/models/users.py
+++ app/models/users.py
@@ -11,9 +11,7 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(250))
     mail = db.Column(db.String(25), unique=True)
     profile_image = db.Column(db.String(120), default="champi_1.jpg")
-
-    likes = db.relationship('Referentiel', secondary=user_likes, backref=db.backref('liked_by', lazy='dynamic'))
-
+    champi_id = db.Column(db.Integer, db.ForeignKey('referentiel.taxref_id'))
 
     quiz_comestible_scores = db.relationship("ScoreQuizComestible", backref="quiz_comestibles_scores", lazy=True)
 
